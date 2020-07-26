d8233e8f56e87f9418818bea392b706e2bd5b7af
Marcelo Ojeda <alphacba@yahoo.com.br>
2020-07-26 16:04:57 -0300

    Modificando arquivo cloudbuild.

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-echo "composer install"
 composer install
-
-echo "artisan config:clear"
 php artisan config:clear
-
-echo "artisan optimize:clear"
 php artisan optimize:clear
-
 php artisan key:generate
-
-echo "artisan config:cache"
 php artisan config:cache
-
-echo "artisan migrate"
 php artisan migrate
-
-echo "php-fpm"
 php-fpm
-
-echo "Run PHPUnit Tests"
 ./vendor/bin/phpunit
